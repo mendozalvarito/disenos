d94f15008e0d663fe5df4e01fd322d216430304f
mendozalvarito laptop <mendozalvarito@gmail.com>
2021-01-29 23:37:48 -0400

    ocultar sidebar

---
+++ dist/js/script.js
@@ -0,0 +1,4 @@
+document.getElementsByClassName("sidebar_btn")[0].onclick=clickAside();
+function clickAside(){
+    document.getElementsByClassName("grid-container")[0].classList.add("collapse");
+}
